58a50700cc548f17229223dec56d73d3ed0dea58
Alain Dekker <alain.dekker@teraki.com>
2019-10-26 15:13:29 +0200

    Initial version from template - thanks Sivan!

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    cout << "argc=" << argc << endl;
+    cout << "program=" << argv[0] << endl;
+    return 0;
+}
